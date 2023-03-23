9ec468c8ea1d3ebfeaca77195a92b46cb5b3e7f8
kingcharlezz <39387371+kingcharlezz@users.noreply.github.com>
2023-03-22 23:34:01 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env perl
+#!/usr/bin/env python3
 
 # Author: philsmd
 # Date: July 2021
@@ -9,82 +9,108 @@
 
 # only tested with version 3 of a PolkaWallet test wallet
 
-use strict;
-use warnings;
+from base64 import b64decode
 
-use MIME::Base64     qw (decode_base64);
-use Crypt::ScryptKDF qw (scrypt_raw);
-use Crypt::Sodium    qw (crypto_secretbox_open);
-# Unfortunately, Crypt::NaCl::Sodium seems to be outdated (and not compiling)
+import sys
+import struct
+import scrypt # py-scrypt (or use hashlib.scrypt or passlib.hash.scrypt)
+
+from nacl.secret import SecretBox # install PyNaCl
 
 #
 # Constants
 #
 
-my $SCRYPT_DEFAULT_N = 32768; # 1 << 15 (2 ^ 15)
-my $SCRYPT_DEFAULT_P =     1;
-my $SCRYPT_DEFAULT_R =     8;
+SCRYPT_DEFAULT_N = 32768 # 1 << 15 (2^15)
+SCRYPT_DEFAULT_P =     1
+SCRYPT_DEFAULT_R =     8
 
 
 #
 # Examples
 #
 
-const PAIR = '{"address":"FLiSDPCcJ6auZUGXALLj6jpahcP6adVFDBUQznPXUQ7yoqH","encoded":"ILjSgYaGvq1zaCz/kx+aqfLaHBjLXz0Qsmr6RnkOVU4AgAAAAQAAAAgAAAB5R2hm5kgXyc0NQYFxvMU4zCdjB+ugs/ibEooqCvuudbaeKn3Ee47NkCqU1ecOJV+eeaVn4W4dRvIpj5kGmQOGsewR+MiQ/B0G9NFh7JXV0qcPlk2QMNW1/mbJrTO4miqL448BSkP7ZOhUV6HFUpMt3B9HwjiRLN8RORcFp0ID/Azs4Jl/xOpXNzbgQGIffWgCIKTxN9N1ku6tdlG4","encoding":{"content":["pkcs8","sr25519"],"type":["scrypt","xsalsa20-poly1305"],"version":"3"},"meta":{"genesisHash":"0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe","name":"version3","tags":[],"whenCreated":1595277797639,"whenEdited":1595278378596}}';
-my $PASS    = "version3";
+# const PAIR = '{"address":"FLiSDPCcJ6auZUGXALLj6jpahcP6adVFDBUQznPXUQ7yoqH","encoded":"ILjSgYaGvq1zaCz/kx+aqfLaHBjLXz0Qsmr6RnkOVU4AgAAAAQAAAAgAAAB5R2hm5kgXyc0NQYFxvMU4zCdjB+ugs/ibEooqCvuudbaeKn3Ee47NkCqU1ecOJV+eeaVn4W4dRvIpj5kGmQOGsewR+MiQ/B0G9NFh7JXV0qcPlk2QMNW1/mbJrTO4miqL448BSkP7ZOhUV6HFUpMt3B9HwjiRLN8RORcFp0ID/Azs4Jl/xOpXNzbgQGIffWgCIKTxN9N1ku6tdlG4","encoding":{"content":["pkcs8","sr25519"],"type":["scrypt","xsalsa20-poly1305"],"version":"3"},"meta":{"genesisHash":"0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe","name":"version3","tags":[],"whenCreated":1595277797639,"whenEdited":1595278378596}}';
+# PASS3 = "version3";
 
-my $ENCODED = "ILjSgYaGvq1zaCz/kx+aqfLaHBjLXz0Qsmr6RnkOVU4AgAAAAQAAAAgAAAB5R2hm5kgXyc0NQYFxvMU4zCdjB+ugs/ibEooqCvuudbaeKn3Ee47NkCqU1ecOJV+eeaVn4W4dRvIpj5kGmQOGsewR+MiQ/B0G9NFh7JXV0qcPlk2QMNW1/mbJrTO4miqL448BSkP7ZOhUV6HFUpMt3B9HwjiRLN8RORcFp0ID/Azs4Jl/xOpXNzbgQGIffWgCIKTxN9N1ku6tdlG4";
+ENCODED = "ILjSgYaGvq1zaCz/kx+aqfLaHBjLXz0Qsmr6RnkOVU4AgAAAAQAAAAgAAAB5R2hm5kgXyc0NQYFxvMU4zCdjB+ugs/ibEooqCvuudbaeKn3Ee47NkCqU1ecOJV+eeaVn4W4dRvIpj5kGmQOGsewR+MiQ/B0G9NFh7JXV0qcPlk2QMNW1/mbJrTO4miqL448BSkP7ZOhUV6HFUpMt3B9HwjiRLN8RORcFp0ID/Azs4Jl/xOpXNzbgQGIffWgCIKTxN9N1ku6tdlG4"
 
 
 #
 # Start
 #
 
-my $raw_data = decode_base64 ($ENCODED);
+if len (sys.argv) < 2:
+  print ("ERROR: Please specify the dict file within the command line", file=sys.stderr)
 
-my $salt = substr ($raw_data, 0, 32);
+  sys.exit (1)
 
-my $scrypt_n = unpack ("I<", substr ($raw_data, 32, 4));
-my $scrypt_p = unpack ("I<", substr ($raw_data, 36, 4));
-my $scrypt_r = unpack ("I<", substr ($raw_data, 40, 4));
+fp = None
 
-if ($scrypt_n != $SCRYPT_DEFAULT_N)
-{
-  print STDERR "ERROR: Scrypt N value not valid\n";
+try:
+  fp = open (sys.argv[1])
+except:
+  print ("ERROR: Could not open dictionary file '%s'" % sys.argv[1], file=sys.stderr)
 
-  exit (1);
-}
+  sys.exit (1)
 
-if ($scrypt_p != $SCRYPT_DEFAULT_P)
-{
-  print STDERR "ERROR: Scrypt P value not valid\n";
+raw_data = b64decode (ENCODED)
 
-  exit (1);
-}
+salt = raw_data[0:32]
 
-if ($scrypt_r != $SCRYPT_DEFAULT_R)
-{
-  print STDERR "ERROR: Scrypt R value not valid\n";
+scrypt_n = struct.unpack ("<I", raw_data[32:36])[0]
+scrypt_p = struct.unpack ("<I", raw_data[36:40])[0]
+scrypt_r = struct.unpack ("<I", raw_data[40:44])[0]
 
-  exit (1);
-}
+if scrypt_n != SCRYPT_DEFAULT_N:
+  print ("ERROR: Scrypt N value not valid", file=sys.stderr)
 
-my $nonce     = substr ($raw_data, 32 + (3 * 4) +  0, 24);
-my $encrypted = substr ($raw_data, 32 + (3 * 4) + 24);
+  sys.exit (1)
 
-while (my $pass = <>)
-{
-  chomp ($pass);
+if scrypt_p != SCRYPT_DEFAULT_P:
+  print ("ERROR: Scrypt P value not valid", file=sys.stderr)
 
-  my $key = scrypt_raw ($pass, $salt, $SCRYPT_DEFAULT_N, $SCRYPT_DEFAULT_R, $SCRYPT_DEFAULT_P, 32);
+  sys.exit (1)
 
-  my $decrypted = crypto_secretbox_open ($encrypted, $nonce, $key);
+if scrypt_r != SCRYPT_DEFAULT_R:
+  print ("ERROR: Scrypt R value not valid", file=sys.stderr)
 
-  next if (! defined ($decrypted));
+  sys.exit (1)
 
-  print "Password found: '$pass'\n";
+offset = 32 + (3 * 4) # 32 byte salt + 3 numbers (N, p, r)
 
-  exit (0);
-}
+nonce     = raw_data[offset +  0:offset + 24]
+encrypted = raw_data[offset + 24:]
 
-exit (1);
+cracked = False
+
+password = fp.readline ()
+
+while password:
+  key = scrypt.hash (password.strip (), salt, N = SCRYPT_DEFAULT_N, r = SCRYPT_DEFAULT_R, p = SCRYPT_DEFAULT_P, buflen = 32)
+
+  box = SecretBox (key)
+
+  try:
+    box.decrypt (encrypted, nonce)
+
+    print ("Password found: '%s'" % password.strip ())
+
+    cracked = True
+
+    break
+  except:
+    password = fp.readline ()
+
+
+# Cleanup:
+
+fp.close ()
+
+
+# Exit codes:
+
+if cracked:
+  sys.exit (0)
+else:
+  sys.exit (1)
