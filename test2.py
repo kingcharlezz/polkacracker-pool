1403a4348fe325b464b354a01d06da754526a52a
kingcharlezz <39387371+kingcharlezz@users.noreply.github.com>
2023-03-24 13:10:02 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,4 +1,3 @@
-import os
 from base64 import b64decode
 import sys
 import struct
@@ -8,16 +7,6 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import threading
 from threading import Lock
 
-def save_checkpoint(line_num):
-    with open("checkpoint.txt", "w") as f:
-        f.write(str(line_num))
-
-def load_checkpoint():
-    if os.path.exists("checkpoint.txt"):
-        with open("checkpoint.txt", "r") as f:
-            return int(f.read().strip())
-    return 0
-
 SCRYPT_DEFAULT_N = 32768
 SCRYPT_DEFAULT_P = 1
 SCRYPT_DEFAULT_R = 8
@@ -25,7 +14,7 @@ SCRYPT_DEFAULT_R = 8
 password_found = False
 password_found_lock = threading.Lock()
 
-ENCODED = "T/ej+6KLpBVeFNaTxx6IVjNxKK3ToqJxti7R/vrg02sAgAAAAQAAAAgAAACCiUOMV0Wp7JrqGtw+IgebYqOMI8Nc81z97BXeCQ/gcji1YgRZICr6236rMy5OPfa6da/Aq8yR4ef7LJ40CFI4an4bIonqg3a40+KMu9QZ+iHVWf39U/TsI+XubAiLrqRcRIBrAq20CEw4TwRjsLYV41spkKocDB4SscxfpwuopUiKEuQ2+unxabsOC3poe4PH161TitOKiXAlXGrW"
+ENCODED = "L//SdVuVLDwr1xTBRdE/36I0uekYanqvVQ/mzBttMh0AgAAAAQAAAAgAAACApnc4Fxc3dxmhdAz2JNwRmNs2wqNvA/KSbfM8Qe16aId2B+Goq1FVscgVSe8FzTu6DONP+el8K8gv4+nx9qxcGBlEoBf2KhUuHo6ZSi7nHNoui5ciT7Yd8WpkIcdwiCeJEcw9huysjfuPVQZXx0KuXODfA3UhW4oICKunifDQ+AkucS0Af+l1kPGpMM5uSZbcx8BYtxe9D9UtzrXL"
 
 counter_lock = Lock()
 counter = 0
@@ -56,14 +45,10 @@ def process_line(line, salt, nonce, encrypted):
     password = line.strip()
     current_line = update_counter()
     print(f"Checking password at line {current_line}", end="\r")
-    try:
-        result = try_decrypt(password, salt, nonce, encrypted)
-        if result:
-            print(f"Password found: '{result}'")
-        return result
-    except Exception as e:
-        save_checkpoint(current_line)
-        print(f"Error at line {current_line}: {e}", file=sys.stderr)
+    result = try_decrypt(password, salt, nonce, encrypted)
+    if result:
+        print(f"Password found: '{result}'")
+    return result
 
 def main():
     if len(sys.argv) < 2:
@@ -86,17 +71,23 @@ def main():
     offset = 32 + (3 * 4)
     nonce = raw_data[offset:offset + 24]
     encrypted = raw_data[offset + 24:]
-    
-    start_line = load_checkpoint()
-    if start_line > 0:
-        print(f"Resuming from line {start_line}")
-        for _ in range(start_line):
-            next(fp)
 
     cracked = False
+    
+    num_processes = 8  # Change this value to modify the number of processes
+    
+    # Load the last line number from a file, or start at 0 if the file doesn't exist
+    try:
+        with open('last_line', 'r') as f:
+        last_line = int(f.readline())
+except:
+    last_line = 0
 
-    num_processes = 16  # Change this value to modify the number of processes
+# Seek to the last line that was successfully processed and start from there
+fp.seek(last_line)
 
+# Create a new file to store the line number when the program stops
+with open('last_line.txt', 'w') as f:
     with ThreadPoolExecutor(max_workers=num_processes) as executor:
         futures = [executor.submit(process_line, line, salt, nonce, encrypted) for line in fp]
 
@@ -105,15 +96,19 @@ def main():
             if result:
                 cracked = True
                 break
+            
+            # Save the current line number to the file every 1000 lines
+            if update_counter() % 1000 == 0:
+                f.write(str(fp.tell()) + '\n')
 
-    fp.close()
+    # Save the final line number to the file
+    f.write(str(fp.tell()) + '\n')
 
-    if cracked:
-        print('cracked')
-        sys.exit(0)
-    else:
-        print('not cracked')
-        sys.exit(1)
+fp.close()
 
-if __name__ == "__main__":
-    main()
+if cracked:
+    print('cracked')
+    sys.exit(0)
+else:
+    print('not cracked')
+    sys.exit(1)
