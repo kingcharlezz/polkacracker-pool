acbee65494a2836b8454d7f7ca7cd3fe7e94a8e7
kingcharlezz <39387371+kingcharlezz@users.noreply.github.com>
2023-03-23 22:43:21 -0400

    Update polkacracker.py

--- polkacracker.py
+++ polkacracker.py
@@ -11,7 +11,7 @@ SCRYPT_DEFAULT_N = 32768
 SCRYPT_DEFAULT_P = 1
 SCRYPT_DEFAULT_R = 8
 
-ENCODED = "L//SdVuVLDwr1xTBRdE/36I0uekYanqvVQ/mzBttMh0AgAAAAQAAAAgAAACApnc4Fxc3dxmhdAz2JNwRmNs2wqNvA/KSbfM8Qe16aId2B+Goq1FVscgVSe8FzTu6DONP+el8K8gv4+nx9qxcGBlEoBf2KhUuHo6ZSi7nHNoui5ciT7Yd8WpkIcdwiCeJEcw9huysjfuPVQZXx0KuXODfA3UhW4oICKunifDQ+AkucS0Af+l1kPGpMM5uSZbcx8BYtxe9D9UtzrXL"
+ENCODED = "6YQ09y3SrOBIzgUqvV7N47q/jKHbHa2aKUqQCpq77KIAgAAAAQAAAAgAAABux0VeXlE/TOqqw2izAt7Hy5sh+B99q+BMNHU6NIUCev7mNmwV4wICnz0rEEv2ll4i28mfTlZpbzDlP0KHikztX3WHscVKjAwy88jBZ4FXLWmShPkQkI8Nf2JxToG4OnwwMv24dMKjvaCKN1mglPjmfhkLVwzl+bgeCH2DTaJfW9oDW2sjwFq7IznXcTfk2njIFTUpIrlVboqoaZml"
 
 password_found = False
 password_found_lock = threading.Lock()
@@ -22,7 +22,7 @@ def try_decrypt(password, salt, nonce, encrypted, index):
         if password_found:
             return None
 
-    print(f"Checking password at line {index}", end="\r")
+    print(f"Checking password at line {index}")
     key = scrypt.hash(password.strip(), salt, N=SCRYPT_DEFAULT_N, r=SCRYPT_DEFAULT_R, p=SCRYPT_DEFAULT_P, buflen=32)
     box = SecretBox(key)
     try:
@@ -39,15 +39,14 @@ def main():
         sys.exit(1)
 
     try:
-        with open(sys.argv[1]) as fp:
-            passwords = fp.readlines()
+        fp = open(sys.argv[1], 'r')
     except:
         print("ERROR: Could not open dictionary file '%s'" % sys.argv[1], file=sys.stderr)
         sys.exit(1)
 
     raw_data = b64decode(ENCODED)
 
-    salt = raw_data[0:32]
+     salt = raw_data[0:32]
     scrypt_n = struct.unpack("<I", raw_data[32:36])[0]
     scrypt_p = struct.unpack("<I", raw_data[36:40])[0]
     scrypt_r = struct.unpack("<I", raw_data[40:44])[0]
@@ -58,17 +57,26 @@ def main():
 
     cracked = False
 
-    num_processes = 4  # Change this value to modify the number of processes
+    num_processes = 6  # Change this value to modify the number of processes
 
     with ThreadPoolExecutor(max_workers=num_processes) as executor:
-        futures = [executor.submit(try_decrypt, passwords[i], salt, nonce, encrypted, i+1) for i in range(len(passwords))]
+        futures = []
 
-        for future in as_completed(futures):
-            result = future.result()
-            if result:
-                print("Password found: '%s'" % result)
-                cracked = True
-                break
+        for index, password in enumerate(fp):
+            future = executor.submit(try_decrypt, password, salt, nonce, encrypted, index + 1)
+            futures.append(future)
+
+            # Cancel remaining tasks if password is found
+            for future in as_completed(futures):
+                result = future.result()
+                if result:
+                    print("Password found: '%s'" % result)
+                    cracked = True
+                    for future in futures:
+                        future.cancel()
+                    break
+
+    fp.close()
 
     if cracked:
         print('cracked')
