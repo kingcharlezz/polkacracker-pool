9e38ac72e856511a7b7bd0c0d2b9bf802cd2ddf3
kingcharlezz <39387371+kingcharlezz@users.noreply.github.com>
2023-03-23 22:15:53 -0400

    Update polkacracker.py

--- polkacracker.py
+++ polkacracker.py
@@ -1,119 +1,81 @@
-#!/usr/bin/env python3
-
-# Author: philsmd
-# Date: July 2021
-# License: public domain, credits go to philsmd and hashcat
-
-# Note: NaCl uses XSalsa20 and Poly1305 for decrypting the data.
-# Key derivation is done by scrypt (32768, 8, 1)
-
-# only tested with version 3 of a PolkaWallet test wallet
 
 from base64 import b64decode
-
 import sys
 import struct
-import scrypt # py-scrypt (or use hashlib.scrypt or passlib.hash.scrypt)
+import scrypt
+from nacl.secret import SecretBox
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import threading
 
-from nacl.secret import SecretBox # install PyNaCl
-import multiprocessing
+SCRYPT_DEFAULT_N = 32768
+SCRYPT_DEFAULT_P = 1
+SCRYPT_DEFAULT_R = 8
 
-#
-# Constants
-#
+ENCODED = "L//SdVuVLDwr1xTBRdE/36I0uekYanqvVQ/mzBttMh0AgAAAAQAAAAgAAACApnc4Fxc3dxmhdAz2JNwRmNs2wqNvA/KSbfM8Qe16aId2B+Goq1FVscgVSe8FzTu6DONP+el8K8gv4+nx9qxcGBlEoBf2KhUuHo6ZSi7nHNoui5ciT7Yd8WpkIcdwiCeJEcw9huysjfuPVQZXx0KuXODfA3UhW4oICKunifDQ+AkucS0Af+l1kPGpMM5uSZbcx8BYtxe9D9UtzrXL"
 
-SCRYPT_DEFAULT_N = 32768 # 1 << 15 (2^15)
-SCRYPT_DEFAULT_P =     1
-SCRYPT_DEFAULT_R =     8
+password_found = False
+password_found_lock = threading.Lock()
 
+def try_decrypt(password, salt, nonce, encrypted, index):
+    global password_found
+    with password_found_lock:
+        if password_found:
+            return None
 
-#
-# Functions
-#
+    print(f"Checking password at line {index}", end="\r")
+    key = scrypt.hash(password.strip(), salt, N=SCRYPT_DEFAULT_N, r=SCRYPT_DEFAULT_R, p=SCRYPT_DEFAULT_P, buflen=32)
+    box = SecretBox(key)
+    try:
+        box.decrypt(encrypted, nonce)
+        with password_found_lock:
+            password_found = True
+        return password.strip()
+    except:
+        return None
 
-def crack_password(password):
-    global cracked
-    global count
-    
-    if not cracked:
-        key = scrypt.hash(password.strip(), salt, N=SCRYPT_DEFAULT_N, r=SCRYPT_DEFAULT_R, p=SCRYPT_DEFAULT_P, buflen=32)
+def main():
+    if len(sys.argv) < 2:
+        print("ERROR: Please specify the dict file within the command line", file=sys.stderr)
+        sys.exit(1)
 
-        box = SecretBox(key)
+    try:
+        with open(sys.argv[1]) as fp:
+            passwords = fp.readlines()
+    except:
+        print("ERROR: Could not open dictionary file '%s'" % sys.argv[1], file=sys.stderr)
+        sys.exit(1)
 
-        try:
-            box.decrypt(encrypted, nonce)
+    raw_data = b64decode(ENCODED)
 
-            print("Password found: '%s'" % password.strip())
-            cracked = True
+    salt = raw_data[0:32]
+    scrypt_n = struct.unpack("<I", raw_data[32:36])[0]
+    scrypt_p = struct.unpack("<I", raw_data[36:40])[0]
+    scrypt_r = struct.unpack("<I", raw_data[40:44])[0]
 
-        except:
-            count += 1
-            if count % 100 == 0: # print progress every 100 attempts
-                print("Tried %d passwords..." % count)
+    offset = 32 + (3 * 4)
+    nonce = raw_data[offset:offset + 24]
+    encrypted = raw_data[offset + 24:]
 
+    cracked = False
 
-#
-# Examples
-#
+    num_processes = 4  # Change this value to modify the number of processes
 
-ENCODED = "xxx";
+    with ThreadPoolExecutor(max_workers=num_processes) as executor:
+        futures = [executor.submit(try_decrypt, passwords[i], salt, nonce, encrypted, i+1) for i in range(len(passwords))]
 
-#
-# Start
-#
+        for future in as_completed(futures):
+            result = future.result()
+            if result:
+                print("Password found: '%s'" % result)
+                cracked = True
+                break
 
-if len(sys.argv) < 2:
-    print("ERROR: Please specify the dict file within the command line", file=sys.stderr)
-    sys.exit(1)
+    if cracked:
+        print('cracked')
+        sys.exit(0)
+    else:
+        print('not cracked')
+        sys.exit(1)
 
-fp = None
-
-try:
-    fp = open(sys.argv[1])
-except:
-    print("ERROR: Could not open dictionary file '%s'" % sys.argv[1], file=sys.stderr)
-    sys.exit(1)
-
-raw_data = b64decode(ENCODED)
-
-salt = raw_data[0:32]
-
-scrypt_n = struct.unpack("<I", raw_data[32:36])[0]
-scrypt_p = struct.unpack("<I", raw_data[36:40])[0]
-scrypt_r = struct.unpack("<I", raw_data[40:44])[0]
-
-if scrypt_n != SCRYPT_DEFAULT_N:
-    print("ERROR: Scrypt N value not valid", file=sys.stderr)
-    sys.exit(1)
-
-if scrypt_p != SCRYPT_DEFAULT_P:
-    print("ERROR: Scrypt P value not valid", file=sys.stderr)
-    sys.exit(1)
-
-if scrypt_r != SCRYPT_DEFAULT_R:
-    print("ERROR: Scrypt R value not valid", file=sys.stderr)
-    sys.exit(1)
-
-offset = 32 + (3 * 4) # 32 byte salt + 3 numbers (N, p, r)
-
-nonce     = raw_data[offset +  0:offset + 24]
-encrypted = raw_data[offset + 24:]
-
-cracked = False
-count = 0
-
-passwords = fp.readlines()
-
-with multiprocessing.Pool() as pool:
-    pool.map(crack_password, passwords)
-
-# Cleanup:
-fp.close()
-
-# Exit codes:
-if cracked:
-    print('cracked')
-    sys.exit(0)
-else:
-    print('not cracked')
-    sys.exit(1)
+if __name__ == "__main__":
+    main()
